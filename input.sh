180eadfab8168fb91a4d77c0be34f9881481cb73
manigandan-d <manigandan220916@gmail.com>
2024-01-24 17:18:25 +0530

    read input from user

---
+++ input.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+# echo "Enter your name: "
+# read name
+# echo "Hi $name"
+
+# read -p "Enter your name: " name
+# read -sp "Enter your password: " passwd
+# echo
+# echo "username: $name"
+# echo "password: $passwd"
+
+# echo "Enter your friend names: "
+# read name1 name2 name3
+# echo "Hi $name1 $name2 $name3"
+
+# echo "Enter your friend names: "
+# read -a names
+# echo $names
+# echo ${names[0]} ${names[1]} ${names[2]}
+
+read -p "Enter your name: "
+echo "Hi $REPLY"
