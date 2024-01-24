89caf9e1ad77a198b1882f59f2fc064ac468e11b
manigandan-d <manigandan220916@gmail.com>
2024-01-24 14:20:27 +0530

    Variables

---
+++ variables.sh
@@ -0,0 +1,16 @@
+#! /bin/bash 
+
+# System defined variables 
+
+echo $SHELL 
+echo $BASH
+echo $BASH_VERSION 
+echo $USERNAME
+echo $HOME
+echo $PWD
+
+# User defined variables 
+
+course="LSS"
+echo $course
+
