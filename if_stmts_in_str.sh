d21c6d3a413213fa0ba769109e58b6f5d7f6e130
manigandan-d <manigandan220916@gmail.com>
2024-01-25 11:08:16 +0530

    if statements in string

---
+++ if_stmts_in_str.sh
@@ -0,0 +1,20 @@
+a="mani"
+b="anbu"
+
+if [ $a == $b ]
+# if [ $a = $b ] 
+then 
+    echo "a and b are equal"
+
+elif [[ $a < $b ]]
+then 
+    echo "a is less than b"
+
+elif [[ $a > $b ]]
+then 
+    echo "a is greater than b"
+
+elif [ $a != $b ]
+then 
+    echo "a and b are not equal"
+fi
