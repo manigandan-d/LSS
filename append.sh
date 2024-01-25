56bd5b2695c076a65376c419605f6aeea125145b
manigandan-d <manigandan220916@gmail.com>
2024-01-25 13:05:46 +0530

    append text into the file

---
+++ append.sh
@@ -0,0 +1,12 @@
+read -p "Enter the file name: " fname
+
+if [ -f $fname ]; then 
+    if [ -w $fname ]; then 
+        echo "Type some text. To quit, press Ctrl+D"
+        cat >> $fname
+    else
+        echo "$fname hasn't write permission"
+    fi
+else
+    echo "$fname not exist"
+fi
