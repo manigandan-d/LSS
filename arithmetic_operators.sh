47bc8a112fbbce671a6a6bde62a2e9e5fb6a43e8
manigandan-d <manigandan220916@gmail.com>
2024-01-26 16:20:21 +0530

    updated

--- arithmetic_operators.sh
+++ arithmetic_operators.sh
@@ -4,27 +4,27 @@ read -p "Enter the value of a: " a
 read -p "Enter the value of b: " b
 
 # c=$(( a+b ))
-# c=$(expr $a + $b )
+# c=$(expr $a + $b)
 c=`expr $a + $b`
 echo "add: $c"
 
 # c=$(( a-b ))
-# c=$(expr $a - $b )
+# c=$(expr $a - $b)
 c=`expr $a - $b`
 echo "sub: $c"
 
 # c=$(( a*b ))
-# c=$(expr $a \* $b )
+# c=$(expr $a \* $b)
 c=`expr $a \* $b`
 echo "mul: $c"
 
 # c=$(( a/b ))
-# c=$(expr $a / $b )
+# c=$(expr $a / $b)
 c=`expr $a / $b`
 echo "div: $c"
 
 # c=$(( a%b ))
-# c=$(expr $a % $b )
+# c=$(expr $a % $b)
 c=`expr $a % $b`
 echo "mod: $c"
 
