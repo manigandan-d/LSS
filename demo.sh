ba0bd3d4d20c9e3c002f36dcec997bf4dbfa68df
manigandan-d <manigandan220916@gmail.com>
2024-02-20 13:08:27 +0530

    updated

--- demo.sh
+++ demo.sh
@@ -1,9 +1,34 @@
 #! /bin/bash
 
-i=1
+echo "Enter your name: "
+read name
+echo "Hi $name"
 
-while [ $i -le 10 ]; do 
-    echo $i
-    ((i++))
+read -p "Enter your name: " name
+echo "Hi $name"
 
-done
+echo -e "Enter your name: \c"
+read name
+echo "Hi $name"
+
+echo -n "Enter your name: "
+read name
+echo "Hi $name"
+
+read -p "Enter your name: " name
+read -sp "Enter your password: " passwd
+echo
+echo "username: $name"
+echo "password: $passwd"
+
+echo "Enter your friend names: "
+read name1 name2 name3
+echo "Hi $name1 $name2 $name3"
+
+echo "Enter your friend names: "
+read -a names
+echo $names
+echo ${names[0]} ${names[1]} ${names[2]}
+
+read -p "Enter your name: "
+echo "Hi $REPLY"
