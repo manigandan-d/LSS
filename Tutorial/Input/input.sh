cb5379b4d750a417283231545245a5b3192a5e9d
manigandan-d <manigandan220916@gmail.com>
2024-02-20 15:24:55 +0530

    input

--- Tutorial/Input/input.sh
+++ Tutorial/Input/input.sh
@@ -1,31 +1,31 @@
 #! /bin/bash
 
-# echo "Enter your name: "
-# read name
-# echo "Hi $name"
+# echo "What's your favourite color: "
+# read color
+# echo "Wow! $color is a nice color"
 
-# read -p "Enter your name: " name
-# echo "Hi $name"
+# read -p "Enter your favourite fruit [default: apple]: " fruit
+# fruit=${fruit:-apple}
+# echo "Your favourite fruit is $fruit"
 
-# echo -e "Enter your name: \c"
-# read name
-# echo "Hi $name"
+# echo -e "Enter your age: \c"
+# read age
+# echo "You are $age years old"
 
-# echo -n "Enter your name: "
-# read name
-# echo "Hi $name"
+# echo -n "Enter the name of the file you want to read: "
+# read fname
+# cat $fname
 
-# read -p "Enter your name: " name
-# read -sp "Enter your password: " passwd
+# read -p "Username: " name
+# read -sp "Password: " passwd
 # echo
-# echo "username: $name"
-# echo "password: $passwd"
+# echo "Accecpted. Processing..."
 
-# echo "Enter your friend names: "
-# read name1 name2 name3
-# echo "Hi $name1 $name2 $name3"
+# echo "Enter your first name, last name and age: "
+# read fname lname age
+# echo "Hi $fname $lname, you are $age years old"
 
-# echo "Enter your friend names: "
+# echo "Enter your best friend names: "
 # read -a names
 # echo $names
 # echo ${names[0]} ${names[1]} ${names[2]}
