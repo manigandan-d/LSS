1b37977b42dde3d6fb629b83aefa51b648c7e0d6
manigandan-d <manigandan220916@gmail.com>
2024-02-21 10:29:37 +0530

    check the given string is empty or not

--- Exercises/String empty or not/main.sh
+++ Exercises/String empty or not/main.sh
@@ -1,11 +1,19 @@
 #! /bin/bash
 
 a=""
+# a="bash"
 
-if [ -z $a ]; then
+if [ -z "$a" ]; then
 # if [[ -z $a ]]; then
     echo "a is an empty string"
 
 else 
     echo "a is not an empty string"
+fi
+
+if [ -n "$a" ]; then
+    echo "a is not an empty string"
+
+else 
+    echo "a is an empty string"
 fi
