85df9ff6157a9e9ac1896e38527fc217a871c6d3
manigandan-d <manigandan220916@gmail.com>
2024-02-23 15:39:58 +0530

    read only variables and functions

--- Tutorial/Constant/readonly.sh
+++ Tutorial/Constant/readonly.sh
@@ -6,3 +6,21 @@ echo "$var"
 
 var=20
 echo "$var"
+
+msg() {
+    echo "hello"
+}
+
+msg
+
+readonly -f msg
+
+msg() {
+    echo "hello world"
+}
+
+msg
+
+# readonly # (or) readonly -p
+readonly -p # display both built-in and user defined readonly variable 
+readonly -f # display both built-in and user defined readonly function
