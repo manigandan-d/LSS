4db14ccc24362e6912c1df3eb05caf5046eff3d1
manigandan-d <manigandan220916@gmail.com>
2024-02-25 11:24:03 +0530

    find the average of the given numbers

---
+++ Exercises/Compute the Average/main.sh
@@ -0,0 +1,26 @@
+#! /bin/bash 
+
+# Given N integers, compute their average, rounded to three decimal places.
+
+read n
+
+sum=0
+
+for (( i=0; i<$n; i++ )); do
+    read x
+    # sum=$(( $sum+$x ))
+    ((sum += x))
+done
+
+echo "$sum $n"
+
+avg=`echo "$sum / $n" | bc -l`
+
+printf "%.3f\n" "$avg"
+
+# sample input 1
+# 4
+# 1
+# 2
+# 9
+# 8
