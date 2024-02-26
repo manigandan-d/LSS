2504620c8600ebf1a4a61e2d46d346974b04a604
manigandan-d <manigandan220916@gmail.com>
2024-02-26 15:32:40 +0530

    updated

--- demo.sh
+++ demo.sh
@@ -1,30 +1,6 @@
-# !/bin/bash
+#! /bin/bash
 
-inputs=()
-
-while : ; do
-    read input
-    
-    if [[ -z $input ]]; then
-        break
-    fi
-    
-    # inputs+=("$input")
-    echo "${line:2:1}"
+while read line;
+do
+    echo "${line}" | cut -c3
 done
-
-# while read line;
-# do
-#     echo "${line}" | cut -c3
-# done
-
-# C.B - Cantonment Board/Cantonment 
-# C.M.C – City Municipal Council
-# C.T – Census Town 
-# E.O – Estate Office 
-# G.P - Gram Panchayat 
-# I.N.A – Industrial Notified Area 
-# I.T.S - Industrial Township 
-# M – Municipality 
-# M.B – Municipal Board 
-# M.C – Municipal Committee
