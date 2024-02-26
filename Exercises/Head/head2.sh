59bd8aee0481b2196d7992101f6940b7c00bdceb
manigandan-d <manigandan220916@gmail.com>
2024-02-26 16:58:36 +0530

    head command exercises

---
+++ Exercises/Head/head2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+# In this challenge, we practice using the head command to display the first n characters of a text file. Display the first 20 characters of an input file.
+
+head -c 20 $1
+
+# sample input as file
+# New York is a state in the Northeastern and Mid-Atlantic regions of the United States. 
+# New York is the 27th-most extensive, the third-most populous populated of the 50 United States. 
+# New York is bordered by New Jersey and Pennsylvania to the south.
+# About one third of all the battles of the Revolutionary War took place in New York.
+# Henry Hudson's 1609 voyage marked the beginning of European involvement with the area.
+
