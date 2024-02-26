78d806997157989aba7b5c234d9b3ef2721ddada
manigandan-d <manigandan220916@gmail.com>
2024-02-26 10:12:52 +0530

    updated

--- demo.sh
+++ demo.sh
@@ -1,15 +1,30 @@
-#! /bin/bash
+# !/bin/bash
 
-result=""
+inputs=()
 
-padding=5
-
-# for i in {1..$padding}; do
-for (( i=0; i<$padding; i++ )); do
-    result+="*"
+while : ; do
+    read input
+    
+    if [[ -z $input ]]; then
+        break
+    fi
+    
+    # inputs+=("$input")
+    echo "${line:2:1}"
 done
 
-echo "$result"
+# while read line;
+# do
+#     echo "${line}" | cut -c3
+# done
 
-printf "|%*s%s%*s|\n" $padding "$result" "hello" $padding "$result"
-
+# C.B - Cantonment Board/Cantonment 
+# C.M.C – City Municipal Council
+# C.T – Census Town 
+# E.O – Estate Office 
+# G.P - Gram Panchayat 
+# I.N.A – Industrial Notified Area 
+# I.T.S - Industrial Township 
+# M – Municipality 
+# M.B – Municipal Board 
+# M.C – Municipal Committee
