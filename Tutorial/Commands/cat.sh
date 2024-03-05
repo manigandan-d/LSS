da706297615eed9fadaa160a98e2a328697f9392
manigandan-d <manigandan220916@gmail.com>
2024-03-05 12:58:07 +0530

    cat command

---
+++ Tutorial/Commands/cat.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+
+# cat - view & create the file
+
+# Creation
+cat > emp_names.txt
+# Ctrl+c - save & exit
+# Ctrl+z - suspend
+
+# Append
+cat >> emp_names.txt
+
+# View
+cat emp_names.txt
+
+# Create hidden file
+cat > .hidden_file.txt
+
+# Display multiple files 
+cat frui
