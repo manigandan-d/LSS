d7352fe240f50eb4789ec2320e27fe7c47dae300
manigandan-d <manigandan220916@gmail.com>
2024-03-05 17:43:56 +0530

    cat command

--- Tutorial/Commands/cat.sh
+++ Tutorial/Commands/cat.sh
@@ -17,4 +17,30 @@ cat emp_names.txt
 cat > .hidden_file.txt
 
 # Display multiple files 
-cat frui
+cat animals.txt birds.txt insects.txt
+cat animals.txt - birds.txt - insects.txt # to go to next file info, press Ctrl+d
+
+# Numbering for the data in the file (including empty line)
+cat -n python.txt
+
+# Numbering for the data in the file (excluding empty line)
+cat -b python.txt
+
+# Mention the EOL
+cat -E python.txt
+# cat -nE python.txt
+
+# Tab space (^I)
+cat -T python.txt
+
+# Reduce multiple line to single line
+cat -s python.txt
+# cat -ns python.txt
+
+# Prevention of overwriting of file 
+set -o noclobber
+cat > python.txt
+
+# Allows overwriting of file
+set +o noclobber
+cat > python.txt
