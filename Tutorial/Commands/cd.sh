dc0dc488833a125d81c40a35836d241765df6120
manigandan-d <manigandan220916@gmail.com>
2024-03-05 12:58:18 +0530

    cd command

---
+++ Tutorial/Commands/cd.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+# cd - change directory
+cd mydir
+
+# Move to previous directory
+cd ..
+
+# Move to root directory
+cd /
+
+# Move to home directory
+# cd ~
+cd 
+
+# Move to previous position
+cd -
