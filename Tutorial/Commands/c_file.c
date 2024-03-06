39bd4a83b072b176c03a4174b91736e309b1fa4a
manigandan-d <manigandan220916@gmail.com>
2024-03-06 14:17:25 +0530

    examples

---
+++ Tutorial/Commands/c_file.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+
+int main() {
+	printf("C File\n");
+	
+	return 0;
+}
+
---
+++ Tutorial/Commands/cpp_file.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+
+using namespace std;
+
+int main() {
+	cout<<"CPP File"<<endl;
+	
+	return 0;
+}
