81b61d22555e37eeb3c8bf5ac58f909b70867a4a
manigandan-d <manigandan220916@gmail.com>
2024-03-06 14:15:11 +0530

    mv command

---
+++ Tutorial/Commands/mv.sh
@@ -0,0 +1,12 @@
+#!/bin/bash 
+
+# mv - move the files/directories from source to destination 
+touch test.txt
+mv test.txt User_1/
+
+mkdir dir/
+mv dir/ User_1/
+
+# Rename the file
+touch test.txt
+mv test.txt sample.txt
