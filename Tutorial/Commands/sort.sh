58c841b3761a701d6fb5e8c16c81d78ca72bf567
manigandan-d <manigandan220916@gmail.com>
2024-03-06 15:07:17 +0530

    sort command

--- Tutorial/Commands/sort.sh
+++ Tutorial/Commands/sort.sh
@@ -1,3 +1,26 @@
 #!/bin/bash 
 
+# sort - sort the given file
+sort animals.txt 
 
+# Sort in reverse order
+sort -r animals.txt 
+
+# Redirect the sorted file 
+sort animals.txt 1> sortedAnimals.txt
+
+# Sort the months
+sort months.txt 
+sort -M months.txt 
+
+# Check the file is sorted or not
+sort -c months.txt 
+sort -c sortedAnimals.txt 
+
+# Sort the numbers 
+sort scores.txt 
+sort -n scores.txt 
+
+# File with delimiter
+sort -t '#' -k 2 info.txt
+sort -nt '#' -k 1 info.txt
