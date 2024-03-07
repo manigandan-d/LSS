7a44c970b8ad0c7193148966867a7a01c097bf22
manigandan-d <manigandan220916@gmail.com>
2024-03-07 16:46:23 +0530

    sort command exercises

---
+++ Exercises/Sort/sort3.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+
+# In this challenge, we practice using the sort command to sort input in text or TSV formats.
+# You are given a text file where each line contains a number. The numbers may be either an integer or have decimal places. There will be no extra characters other than the number or the newline at the end of each line. Sort the lines in ascending order - so that the first line holds the numerically smallest number, and the last line holds the numerically largest number.
+
+sort -n
+
+# Sample Input as file 
+# 9.1
+# 43.7
+# 2.2
+# 62.1
+# 2.1
+# 9.3
+# 43.5
+# 4.6
+# 44.6
+# 4.7
+# 42.7
+# 47.4
+# 46.6
+# 4.5
+# 55.6
+# 4
+# 9.2
+# 66.6
+# 2
+# 2.3
