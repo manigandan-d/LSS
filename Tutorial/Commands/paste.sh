bcb0356d63b2dbe5fd279901ac2f51b553a665cd
manigandan-d <manigandan220916@gmail.com>
2024-03-07 18:12:58 +0530

    paste command

--- Tutorial/Commands/paste.sh
+++ Tutorial/Commands/paste.sh
@@ -1,3 +1,14 @@
 #!/bin/bash 
 
+# paste 
+paste states.txt capitals.txt
 
+# Delimiter
+paste -d "-" states.txt capitals.txt
+paste -d "-" - - - < states.txt 
+
+# Serial
+paste -s states.txt capitals.txt
+
+# Serial with delimiter
+paste -s -d ":" states.txt capitals.txt
