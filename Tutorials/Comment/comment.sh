4b118b5c2dbba4fb23cce48915d02734407e9b79
manigandan-d <manigandan220916@gmail.com>
2025-04-19 16:37:05 +0530

    added comment script showing single and multi-line comments

--- Tutorials/Comment/comment.sh
+++ Tutorials/Comment/comment.sh
@@ -1,6 +1,32 @@
-#! /bin/bash 
+#!/bin/bash
 
-# comment
+# comment.sh - Demonstrates single-line and multi-line comments in Bash
 
-echo "When I wrote this code only God and I understood what it did.  Now, only God knows"
+# Single-line comment
 
+# This script greets the user
+name="John"  # Assigning value to a variable
+echo "Hello, $name"  # Printing greeting
+
+# Multi-line Comments
+
+# Method 1: Using '#' on each line
+
+# This is a multi-line comment
+# describing what this script does.
+# It can span several lines.
+# Each line must start with a `#`.
+
+echo "Hello, world!"
+
+# Method 2: Using a Here Document with ':' (null command)
+
+: <<'COMMENT'
+This is a multi-line comment.
+You can use it to explain parts of your script
+or temporarily disable a block of code.
+
+Everything between COMMENT and COMMENT is ignored.
+COMMENT
+
+echo "This will run."
