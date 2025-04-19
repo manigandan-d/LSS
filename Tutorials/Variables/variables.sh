4341142a8c505a1260de6683aff40cfd553d945d
manigandan-d <manigandan220916@gmail.com>
2025-04-19 16:15:47 +0530

    added variables script with environment and user-defined examples

--- Tutorials/Variables/variables.sh
+++ Tutorials/Variables/variables.sh
@@ -1,16 +1,13 @@
-#! /bin/bash 
+#!/bin/bash
 
-# System defined variables 
+# variables.sh - Demonstrates usage of environment and user-defined variables
 
-echo $SHELL 
-echo $BASH
-echo $BASH_VERSION 
-echo $USERNAME
-echo $HOME
-echo $PWD
+# Environment Variables (predefined by the system)
+echo "Current Shell: $SHELL"
+echo "Bash Version: $BASH_VERSION"
+echo "Home Directory: $HOME"
+echo "Present Working Directory: $PWD"
 
-# User defined variables 
-
-course="LSS"
-echo $course
-
+# User-defined Variable
+name="Linux"
+echo "Welcome to $name shell scripting!"
