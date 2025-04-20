732d3876ef6411b520a40b7a07d981ba30fb733d
manigandan-d <manigandan220916@gmail.com>
2025-04-20 12:36:19 +0530

    add time-based greeting example using if-elif-else

--- Tutorials/Conditionals/if_elif.sh
+++ Tutorials/Conditionals/if_elif.sh
@@ -1,16 +1,17 @@
-#! /bin/bash
+#!/bin/bash
 
+# if_elif.sh - Prints a greeting based on the current hour
+
+# Get the current hour (24-hour format)
 hour=$(date +%H)
 
+# Use if-elif-else to determine the time of day
 if [ $hour -ge 5 ] && [ $hour -lt 12 ]; then
     echo "Good morning!"
-
 elif [ $hour -ge 12 ] && [ $hour -lt 18 ]; then
     echo "Good afternoon!"
-
 elif [ $hour -ge 18 ] && [ $hour -lt 22 ]; then
     echo "Good evening!"
-
 else
     echo "Good night!"
 fi
